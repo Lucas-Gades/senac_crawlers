31cc2fdaf4415502886a0c461db250ef9e730419
Lucas Gades <70823238+Lucas-Gades@users.noreply.github.com>
2024-10-09 20:23:13 -0300

    Update urls.py

--- api-senac-crawlers/api_rest/urls.py
+++ api-senac-crawlers/api_rest/urls.py
@@ -7,13 +7,18 @@ urlpatterns = [
 
     # CRUD para Edital
     path('editais/', views.edital_list, name='edital-list'),  # Listar e criar editais
-    path('editais/deletar/<int:pk>/', views.edital_detail, name='edital-detail'),  # Detalhar e deletar edital específico
+    path('editais/deletar/<int:pk>/', views.edital_list, name='edital-detail'),  # Detalhar e deletar edital específico
+    path('editais/atualizar/<int:pk>/', views.edital_list, name='edital-update'),  # Atualizar edital específico
+
 
     # CRUD para Site
     path('sites/', views.site_list, name='site-list'),  # Listar e criar sites
-    path('sites/deletar/<int:pk>/', views.site_detail, name='site-detail'),  # Detalhar e deletar site específico
+    path('sites/deletar/<int:pk>/', views.site_list, name='site-detail'),  # Detalhar e deletar site específico
+    path('sites/atualizar/<int:pk>/', views.site_list, name='site-update'),  # Atualizar site específico
 
     # CRUD para Usuario
     path('usuarios/', views.usuario_list, name='usuario-list'),  # Listar e criar usuários
-    path('usuarios/deletar/<int:pk>/', views.usuario_detail, name='usuario-detail'),  # Detalhar e deletar usuário específico
+    path('usuarios/deletar/<int:pk>/', views.usuario_list, name='usuario-detail'),  # Detalhar e deletar usuário específico
+    path('usuarios/atualizar/<int:pk>/', views.usuario_list, name='usuario-update'),  # Atualizar usuário específico
+
 ]
--- api-senac-crawlers/api_rest/views.py
+++ api-senac-crawlers/api_rest/views.py
@@ -1,17 +1,15 @@
-from rest_framework import  status
+from rest_framework import status
 from rest_framework.response import Response
 from .models import Edital, Site, Usuario
 from .serializers import EditalSerializer, SiteSerializer, UsuarioSerializer
 from rest_framework.decorators import api_view
-
-
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Seja bem-vindo à API Senac Crawlers! :))")
 
 # CRUD para Edital
-@api_view(['GET', 'POST', 'PUT'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def edital_list(request):
     if request.method == 'GET':
         editais = Edital.objects.all()
@@ -19,7 +17,6 @@ def edital_list(request):
         return Response(serializer.data)
 
     if request.method == 'POST':
-        # Inserir novo edital
         serializer = EditalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -27,7 +24,6 @@ def edital_list(request):
         return Response({"message": "Erro ao inserir edital.", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'PUT':
-        # Atualizar edital existente
         edital_id = request.data.get('id')
         try:
             edital = Edital.objects.get(pk=edital_id)
@@ -39,23 +35,17 @@ def edital_list(request):
         except Edital.DoesNotExist:
             return Response({"message": "Edital não encontrado."}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET', 'DELETE'])
-def edital_detail(request, pk):
-    try:
-        edital = Edital.objects.get(pk=pk)
-    except Edital.DoesNotExist:
-        return Response({"message": "Edital não encontrado."}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = EditalSerializer(edital)
-        return Response(serializer.data)
-
     if request.method == 'DELETE':
-        edital.delete()
-        return Response({"message": "Edital deletado com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
+        edital_id = request.data.get('id')
+        try:
+            edital = Edital.objects.get(pk=edital_id)
+            edital.delete()
+            return Response({"message": "Edital deletado com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
+        except Edital.DoesNotExist:
+            return Response({"message": "Edital não encontrado."}, status=status.HTTP_404_NOT_FOUND)
 
 # CRUD para Site
-@api_view(['GET', 'POST', 'PUT'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def site_list(request):
     if request.method == 'GET':
         sites = Site.objects.all()
@@ -63,7 +53,6 @@ def site_list(request):
         return Response(serializer.data)
 
     if request.method == 'POST':
-        # Inserir novo site
         serializer = SiteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -71,7 +60,6 @@ def site_list(request):
         return Response({"message": "Erro ao inserir site.", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'PUT':
-        # Atualizar site existente
         site_id = request.data.get('id')
         try:
             site = Site.objects.get(pk=site_id)
@@ -83,23 +71,17 @@ def site_list(request):
         except Site.DoesNotExist:
             return Response({"message": "Site não encontrado."}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET', 'DELETE'])
-def site_detail(request, pk):
-    try:
-        site = Site.objects.get(pk=pk)
-    except Site.DoesNotExist:
-        return Response({"message": "Site não encontrado."}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SiteSerializer(site)
-        return Response(serializer.data)
-
     if request.method == 'DELETE':
-        site.delete()
-        return Response({"message": "Site deletado com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
+        site_id = request.data.get('id')
+        try:
+            site = Site.objects.get(pk=site_id)
+            site.delete()
+            return Response({"message": "Site deletado com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
+        except Site.DoesNotExist:
+            return Response({"message": "Site não encontrado."}, status=status.HTTP_404_NOT_FOUND)
 
 # CRUD para Usuario
-@api_view(['GET', 'POST', 'PUT'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def usuario_list(request):
     if request.method == 'GET':
         usuarios = Usuario.objects.all()
@@ -107,7 +89,6 @@ def usuario_list(request):
         return Response(serializer.data)
 
     if request.method == 'POST':
-        # Inserir novo usuário
         serializer = UsuarioSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -115,7 +96,6 @@ def usuario_list(request):
         return Response({"message": "Erro ao inserir usuário.", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == 'PUT':
-        # Atualizar usuário existente
         usuario_id = request.data.get('id')
         try:
             usuario = Usuario.objects.get(pk=usuario_id)
@@ -127,17 +107,11 @@ def usuario_list(request):
         except Usuario.DoesNotExist:
             return Response({"message": "Usuário não encontrado."}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET', 'DELETE'])
-def usuario_detail(request, pk):
-    try:
-        usuario = Usuario.objects.get(pk=pk)
-    except Usuario.DoesNotExist:
-        return Response({"message": "Usuário não encontrado."}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = UsuarioSerializer(usuario)
-        return Response(serializer.data)
-
     if request.method == 'DELETE':
-        usuario.delete()
-        return Response({"message": "Usuário deletado com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
+        usuario_id = request.data.get('id')
+        try:
+            usuario = Usuario.objects.get(pk=usuario_id)
+            usuario.delete()
+            return Response({"message": "Usuário deletado com sucesso!"}, status=status.HTTP_204_NO_CONTENT)
+        except Usuario.DoesNotExist:
+            return Response({"message": "Usuário não encontrado."}, status=status.HTTP_404_NOT_FOUND)
