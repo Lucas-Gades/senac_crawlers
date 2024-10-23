d8fcfe5cce085cbb08afe4a6b2204f605cde1f44
Lucas Gades <70823238+Lucas-Gades@users.noreply.github.com>
2024-10-23 19:43:45 -0300

    Create Staticfiles

--- api-senac-crawlers/api_root/settings.py
+++ api-senac-crawlers/api_root/settings.py
@@ -125,10 +125,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+# URL onde os arquivos estáticos serão servidos
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Diretório onde o Django vai colocar os arquivos coletados
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Diretórios adicionais onde Django vai procurar por arquivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
