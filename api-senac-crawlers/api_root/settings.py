2f9938b9e7563324a8367fde3406af8665fda259
Lucas Gades <70823238+Lucas-Gades@users.noreply.github.com>
2024-10-23 00:29:55 -0300

    Update settings.py

--- api-senac-crawlers/api_root/settings.py
+++ api-senac-crawlers/api_root/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,16 +128,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Diretório para onde os arquivos estáticos serão coletados
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Diretório onde seus arquivos estáticos estão localizados durante o desenvolvimento
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-# Certifique-se de que DEBUG está desativado no ambiente de produção:
-DEBUG = False
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
